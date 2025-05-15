03babe0bcd7ffc51f69326e163ead6580e26c516
PiotrKujawski <166821212+PiotrKujawski@users.noreply.github.com>
2025-05-15 20:48:09 +0200

    Update circuit.py

--- src/circuit.py
+++ src/circuit.py
@@ -2,7 +2,7 @@ import numpy  as np
 
 # Test version for reference 
 class Circuit:
-    def __init__(self, R: float, R2: float, L: float, C: float) -> None:
+    def __init__(self, R1: float, R2: float, L: float, C: float) -> None:
         """
         Initialize the Circuit class with resistance, inductance, and capacitance values.
         This is a test version of the class. Implement the actual logic in the future.
@@ -13,10 +13,14 @@ class Circuit:
             L (float): Inductance in henries.
             C (float): Capacitance in farads.
         """
-        self.R = R
+        self.R1 = R1
         self.R2 = R2
         self.L = L
         self.C = C 
+        self.omega_range = np.zeros(100, dtype: int8)
+        for i in range(100):
+            self.omega_range(i) = 10^(0.1*(i-30))
+
     
     def response(self, input: np.array, dt: float) -> np.array:
         """
@@ -30,5 +34,27 @@ class Circuit:
         Returns:
             np.array: Simulated response of the RLC circuit.
         """
-        # Placeholder for actual simulation logic
-        return np.sin(input)
+        N = input.size
+
+        output = np.zeros(N, dtype: float)
+
+        self.a = 1/self.R2/self.C
+        self.b = (self.R1 + self.R2)/self.R1*self.a
+
+        for i in range(N):
+            E = input(i) - SUM*self.b
+            SUM = SUM + E* dt
+            output(i) = SUM * self.a
+        return output
+    
+    def Bode_Amplification(self) -> np.array:
+        bode_A =  np.zeros(100, dtype: float)
+        for i in range(100):
+            bode_A(i) = self.a/np.sqrt(self.b^2+self.omega_range(i)^2)
+        return bode_A
+    
+    def Bode_Phase(self) -> np.array:
+        bode_P =  np.zeros(100, dtype: float)
+        for i in range(100):
+            bode_P(i) = -np.arctan2(self.omega_range(i),self.b)
+        return bode_P
